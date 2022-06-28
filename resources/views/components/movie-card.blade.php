257abe4947c990148c0ddb780ea0239ee5c56819
comemaino <mat.maino@gmail.com>
2022-06-28 16:35:56 +0200

    Add: Str::limit on movie->date

--- resources/views/components/movie-card.blade.php
+++ resources/views/components/movie-card.blade.php
@@ -2,7 +2,7 @@
 	<img src="{{ $movie->thumb }}" alt="{{ $movie->title }}">
 	<h3>{{ $movie->title }}</h3>
 	<h5>{{ $movie->original_title }}</h5>
-	<p class="year">{{ $movie->date }}</p>
+	<p class="year">{{ Str::limit($movie->date, 4, $end = '') }}</p>
 	<p
 		class="rating @if ($movie->vote < 5) red
     @elseif ($movie->vote < 7) yellow 
