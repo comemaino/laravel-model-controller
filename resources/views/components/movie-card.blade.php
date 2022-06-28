af7d2077440b397ab4eb660e091fb545b6842484
comemaino <mat.maino@gmail.com>
2022-06-28 16:16:05 +0200

    Add: conditional style on movie->vote

--- resources/views/components/movie-card.blade.php
+++ resources/views/components/movie-card.blade.php
@@ -3,5 +3,9 @@
 	<h3>{{ $movie->title }}</h3>
 	<h5>{{ $movie->original_title }}</h5>
 	<p class="year">{{ $movie->date }}</p>
-	<p class="rating">{{ $movie->vote }}</p>
+	<p
+		class="rating @if ($movie->vote < 5) red
+    @elseif ($movie->vote < 7) yellow 
+    @else green @endif">
+		{{ $movie->vote }}</p>
 </div>
